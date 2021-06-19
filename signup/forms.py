8052dd4a11ecf1ef1c3405c1da708f7d2fd62b6c
tuhin-thinks <tuhinmitra190221@gmail.com>
2021-06-19 22:47:59 +0530

    new signup-login page design, user creation-authentication complete, new base.html for signup page-use of jquery

--- signup/forms.py
+++ signup/forms.py
@@ -4,12 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class SignUpForm(UserCreationForm):
-    fname = forms.CharField(widget=forms.TextInput(attrs={'name':'fname', 'required':'true', 'placeholder':'First Name', 'class':'field-input'}))
-    lname = forms.CharField(widget=forms.TextInput(attrs={'name':'lname', 'required':'false', 'onclick':'hint_function()', 'placeholder':'Last Name (optional)', 'class':'field-input'}))
-    username = forms.CharField(label="", widget=forms.TextInput(attrs={'name':'uname', 'required': 'true', 'placeholder':'User Name', 'class':'field-input'}), )
-    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'name':'email', 'placeholder':'Email address', 'required':'true', 'class':'field-input'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'name':'pswd', 'required':'true', 'placeholder': 'Set a new password', 'class':'field-input'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'name':'pswd-repeat', 'required':'true', 'placeholder':'Confirm your password', 'class':'field-input'}))
+    fname = forms.CharField(widget=forms.TextInput(attrs={'name':'fname', 'required':'true', 'placeholder':'First Name', 'class':'field-input', 'value':''}))
+    lname = forms.CharField(widget=forms.TextInput(attrs={'name':'lname', 'required':'false', 'onclick':'hint_function()', 'placeholder':'Last Name (optional)', 'class':'field-input', 'value':''}))
+    username = forms.CharField(label="", widget=forms.TextInput(attrs={'name':'uname', 'required': 'true', 'placeholder':'User Name', 'class':'field-input', 'value':''}))
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'name':'email', 'placeholder':'Email address', 'required':'true', 'class':'field-input', 'value':''}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'name':'pswd', 'required':'true', 'placeholder': 'Set a new password', 'class':'field-input', 'value':''}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'name':'pswd-repeat', 'required':'true', 'placeholder':'Confirm your password', 'class':'field-input', 'value':''}))
 
     class Meta:
         model = User
--- signup/views.py
+++ signup/views.py
@@ -2,9 +2,11 @@ import signup
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from .forms import SignUpForm
+from colorama import Fore
 
 # Create your views here.
 def user_signup(request):
+    form = SignUpForm()
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -16,12 +18,12 @@ def user_signup(request):
             user = authenticate(username=username, password=password_actual)
             if user is not None:
                 login(request, user)
+                print(f"{Fore.LIGHTCYAN_EX}User signed-up{Fore.RESET}")
                 return redirect('profile')
-            
-            return redirect('signup')  # this will redirect user to the same page (if registration fails)
+            form = SignUpForm()
+            return render(request, 'signup', {'forms':form})  # this will redirect user to the same page (if registration fails)
 
     else:
         form = SignUpForm()
 
-
-    return render(request, 'signup/register.html', {'form':form})
+    return render(request, 'signup/register.html', {'forms':form})
